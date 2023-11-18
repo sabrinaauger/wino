85238181494bd90e78104d803f825f7908f09711
Jack Zhong <jakezhong12@gmail.com>
2023-11-18 12:21:21 -0500

    Added final/suggestion page with show additional suggestions and a redo survey

--- app.py
+++ app.py
@@ -1,6 +1,40 @@
 import streamlit as st
 
+# Declare global variables
+global_wine_preference = None
+global_flavour_options = None
+global_dryness_options = None
+
 def main():
+    if 'page' not in st.session_state:
+        st.session_state.page = 'welcome'
+
+    if st.session_state.page == 'welcome':
+        welcome_page()
+    elif st.session_state.page == 'survey':
+        wine_survey_page()
+    elif st.session_state.page == 'result':
+        wine_result_page()
+
+def welcome_page():
+    st.title("Welcome to Wino!")
+    st.write("""
+        Welcome to our Wine Recommender app! This app helps you discover new wines
+        based on your taste preferences.
+
+        **Here's what you can expect:**
+        - Answer a few questions about your wine preferences.
+        - Get a personalized wine recommendation based on your choices.
+        - Discover new wines that match your taste!
+    """)
+    if st.button("Get Started"):
+        st.session_state.page = 'survey'
+
+def wine_survey_page():
+    global global_wine_preference
+    global global_flavour_options
+    global global_dryness_options
+
     st.title("Wine Survey")
 
     # Wine preference section
@@ -17,31 +51,63 @@ def main():
 
     # Submit button
     if st.button("Submit"):
-        st.success("Survey submitted successfully!")
-        display_survey_results(wine_preference, flavour_options, dryness_options)
+        st.session_state.page = 'result'
+        set_global_variables(wine_preference, flavour_options, dryness_options)
 
-def display_survey_results(wine_preference, flavour_options, dryness_options):
-    st.subheader("Survey Results")
-    st.write(f"Wine Preference: {wine_preference}")
-    st.write(f"Flavor Options: {', '.join(flavour_options)}")
-    st.write(f"Dryness Options: {', '.join(dryness_options)}")
+def set_global_variables(wine_preference, flavour_options, dryness_options):
+    global global_wine_preference
+    global global_flavour_options
+    global global_dryness_options
 
-    # Suggested wine based on preferences
-    suggested_wine = suggest_wine(wine_preference, flavour_options, dryness_options)
-    st.subheader("Suggested Wine:")
-    st.write(suggested_wine)
+    global_wine_preference = wine_preference
+    global_flavour_options = flavour_options
+    global_dryness_options = dryness_options
 
-    # Button to request another suggestion
-    if st.button("I do not like this one, show me another similar one"):
-        st.warning("Here's another suggestion:")
-        new_suggestion = suggest_wine(wine_preference, flavour_options, dryness_options)
-        st.write(new_suggestion)
+def suggest_wines():
+    # Access variables from the global scope
+    wine_preference = global_wine_preference
+    flavour_options = global_flavour_options
+    dryness_options = global_dryness_options
 
-def suggest_wine(wine_preference, flavour_options, dryness_options):
-    # Replace this with your logic to suggest a wine based on preferences
-    # You can use external APIs, databases, or any other method to fetch wine suggestions.
-    # For simplicity, a placeholder suggestion is used here.
-    return f"A recommended wine for your preferences: {wine_preference} with {', '.join(flavour_options)} flavors and {', '.join(dryness_options)} dryness."
+    # Replace this with your logic to suggest wines based on preferences
+    # For simplicity, placeholder suggestions are used here.
+    suggestion_parts = [f"A recommended wine for your preferences: {wine_preference}"]
+
+    if flavour_options:
+        suggestion_parts.append(f"with {', '.join(flavour_options)} flavors")
+
+    if dryness_options:
+        suggestion_parts.append(f"and {', '.join(dryness_options)} dryness")
+
+    suggestion1 = ' '.join(suggestion_parts) + "."
+    suggestion2 = "Another suggestion based on your preferences..."
+    suggestion3 = "Yet another suggestion based on your preferences..."
+
+    return [suggestion1, suggestion2, suggestion3]
+
+
+def wine_result_page():
+    # Suggested wines based on preferences
+    suggestions = suggest_wines()
+
+    # Display the first suggestion
+    st.subheader("Suggested Wine 1:")
+    st.write(suggestions[0])
+
+    # Button to show additional suggestions
+    if st.button("Show additional suggestions"):
+        # Display the second suggestion
+        st.subheader("Suggested Wine 2:")
+        st.write(suggestions[1])
+
+        # Display the third suggestion
+        st.subheader("Suggested Wine 3:")
+        st.write(suggestions[2])
+
+    # Redo survey button
+    if st.button("I'm not satisfied. Redo the survey"):
+        st.warning("Survey reset. Please submit your preferences again.")
+        main()
 
 if __name__ == "__main__":
     main()
