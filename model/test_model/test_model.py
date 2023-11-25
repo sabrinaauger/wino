5c8eafb86d6667a579cac10cd9e3331fb93f6ead
Jack Zhong <jakezhong12@gmail.com>
2023-11-25 10:30:16 -0500

    Small model test

--- model/test_model/test_model.py
+++ model/test_model/test_model.py
@@ -1,9 +1,7 @@
 # Example preprocessing using pandas
 import os
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, classification_report
+from sklearn.neighbors import NearestNeighbors
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 
 # Load dataset
@@ -25,28 +23,28 @@ df['variety'] = le.fit_transform(df['variety'])
 scaler = StandardScaler()
 df[['price']] = scaler.fit_transform(df[['price']])
 
-# Extract features and target variable
+# Extract features
 X = df[['country', 'price', 'variety']]
-y = df['suggested_wine']
-
-# Split data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Create and navigate to the model directory
-model_folder = os.path.join(dataset_folder, "test_model")
+model_folder = os.path.join(os.getcwd(), "wine_recommendation_model")
 os.makedirs(model_folder, exist_ok=True)
 os.chdir(model_folder)
 
 # Initialize and train the model
-model = RandomForestClassifier()
-model.fit(X_train, y_train)
+model = NearestNeighbors(n_neighbors=5, algorithm='ball_tree')
+model.fit(X)
 
-# Make predictions
-y_pred = model.predict(X_test)
+# Now, when a user provides their preferences, you can find the nearest wines in the dataset
+user_preferences = pd.DataFrame({'country': ['France'], 'price': [25], 'variety': ['Merlot']})
+user_preferences['country'] = le.transform(user_preferences['country'])
+user_preferences['variety'] = le.transform(user_preferences['variety'])
+user_preferences[['price']] = scaler.transform(user_preferences[['price']])
 
-# Evaluate the model
-accuracy = accuracy_score(y_test, y_pred)
-report = classification_report(y_test, y_pred)
+# Find the nearest wines
+_, indices = model.kneighbors(user_preferences)
 
-print(f"Accuracy: {accuracy}")
-print("Classification Report:\n", report)
+# Display recommended wines
+recommendations = df.iloc[indices[0]]
+print("Recommended Wines:")
+print(recommendations)
